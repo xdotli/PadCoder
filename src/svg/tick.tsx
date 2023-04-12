5eb53e514593ca5931bab6df92947b1f7786f8df
Xiangyi Li <venusli370306@gmail.com>
2023-04-13 01:49:45 +0800

    Update tick.tsx

--- src/svg/tick.tsx
+++ src/svg/tick.tsx
@@ -9,8 +9,8 @@ interface TickSvgProps {
 const TickSvg: React.FC<TickSvgProps> = ({completed}) => {
   const {width, height} = useWindowDimensions();
 
-  const computedWidth = (15 * width) / 1265;
-  const computedHeight = (15 * height) / 1024;
+  const computedWidth = (16 * 1.2 * width) / 1265;
+  const computedHeight = (12.8 * 1.2 * height) / 1024;
   const acXML = `<svg width=${computedWidth} height=${computedHeight} viewBox="0 0 20 17" fill="none" xmlns="http://www.w3.org/2000/svg">
   <path d="M18 2L6.8 14.8L2 10" stroke="#12B022" stroke-width="3" stroke-linecap="round" stroke-linejoin="round"/>
   </svg>`;
@@ -19,10 +19,10 @@ const TickSvg: React.FC<TickSvgProps> = ({completed}) => {
   </svg>`;
 
   if (completed) {
-    return <SvgXml className="ml-4" xml={acXML} />;
+    return <SvgXml className="ml-5 mt-1" xml={acXML} />;
   }
 
-  return <SvgXml className="ml-4" xml={emptyXML} />;
+  return <SvgXml className="ml-5 mt-1" xml={emptyXML} />;
 };
 
 export default TickSvg;
